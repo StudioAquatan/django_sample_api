77a7118e5697c16fe310f59320d5604b9861a059
Pudding <s.kokuryo@gmail.com>
2018-05-28 11:17:10 +0900

    add validation for sound file

--- src/sound/serializers.py
+++ src/sound/serializers.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework import serializers
 
 from .models import Sound
@@ -7,3 +8,10 @@ class SoundSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sound
         fields = ['pk', 'name', 'file', 'created_at']
+
+    def validate_file(self, file):
+        fname = os.path.basename(file.name)
+        _, ext = os.path.splitext(fname)
+        if ext[1:] not in ['mp3', 'wav', 'm4a']:
+            raise serializers.ValidationError('"{ext}" is not a sound file.'.format(ext=ext))
+        return file
